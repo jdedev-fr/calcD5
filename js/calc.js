0e192efaf9f0876892772f9ef5c29c492b656345
JdeDev <julien@jdedev.fr>
2021-09-28 16:51:22 +0200

    gestion des touches = bksp C et .

--- js/calc.js
+++ js/calc.js
@@ -59,3 +59,80 @@ function egal() {
         }
     }
 }
+
+function bksp() {
+    //On met dans le resultat la chaine de resultat en enlevant le dernier caractère
+    resultat.innerHTML = resultat.innerHTML.substr(0, resultat.innerHTML.length - 1)
+}
+
+function appuisC() {
+    //On récupère la position des symboles +-*/ dans la balise HTML résultat, 
+    //si la fonction répond -1 la balise résultat ne contient pas le symbole
+    let pExiste = resultat.innerHTML.indexOf('+')
+    let mExiste = resultat.innerHTML.indexOf('-')
+    let fExiste = resultat.innerHTML.indexOf('X')
+    let dExiste = resultat.innerHTML.indexOf('/')
+
+    if (pExiste == -1 && mExiste == -1 && fExiste == -1 && dExiste == -1) {
+        //on a pas de signe
+        appuisClear()
+    }
+    else {
+        if (pExiste > -1) {
+            //on a un plus, on garde le début
+            resultat.innerHTML = resultat.innerHTML.split('+')[0]
+        } else {
+            if (mExiste > -1) {
+                //on a un moins, on garde le début
+                resultat.innerHTML = resultat.innerHTML.split('-')[0]
+            } else {
+                if (fExiste > -1) {
+                    //on a un fois, on garde le début
+                    resultat.innerHTML = resultat.innerHTML.split('X')[0]
+                } else {
+                    if (dExiste > -1) {
+                        //on a un diviser, on garde le début
+                        resultat.innerHTML = resultat.innerHTML.split('/')[0]
+                    }
+                }
+            }
+        }
+    }
+}
+
+function appuisPt() {
+    //On récupère la position des symboles +-*/ dans la balise HTML résultat, 
+    //si la fonction répond -1 la balise résultat ne contient pas le symbole
+    let pExiste = resultat.innerHTML.indexOf('+')
+    let mExiste = resultat.innerHTML.indexOf('-')
+    let fExiste = resultat.innerHTML.indexOf('X')
+    let dExiste = resultat.innerHTML.indexOf('/')
+
+    if (pExiste > -1) {
+        //On a un plus
+        if (resultat.innerHTML.split('+')[1].indexOf('.') == -1) {
+            resultat.innerHTML += '.'
+        }
+    } else if (mExiste > -1) {
+        //On a un moins
+        if (resultat.innerHTML.split('-')[1].indexOf('.') == -1) {
+            resultat.innerHTML += '.'
+        }
+    } else if (fExiste > -1) {
+        //On a un fois
+        if (resultat.innerHTML.split('X')[1].indexOf('.') == -1) {
+            resultat.innerHTML += '.'
+        }
+    } else if (dExiste > -1) {
+        //On a un diviser
+        if (resultat.innerHTML.split('/')[1].indexOf('.') == -1) {
+            resultat.innerHTML += '.'
+        }
+    } else {
+        //On a pas de signe
+        if (resultat.innerHTML.indexOf('.') == -1) {
+            resultat.innerHTML += '.'
+        }
+    }
+
+}
