217504a45716bfe934894ef2c12d054542fbc5f3
JdeDev <julien@jdedev.fr>
2021-09-28 14:23:39 +0200

    ajout de commentaires

--- js/calc.js
+++ js/calc.js
@@ -1,14 +1,19 @@
+// On attrape la balise HTML qui à pour id resultat
 let resultat = document.getElementById('resultat')
 
 function appuisTouche(num) {
+    //On ajoute le numéro num dans la balise HTML qui a pour id résultat
     resultat.innerHTML += num
 }
 function appuisSymbole(sym) {
+    //On récupère la position des symboles +-*/ dans la balise HTML résultat, 
+    //si la fonction répond -1 la balise résultat ne contient pas le symbole
     let pExiste = resultat.innerHTML.indexOf('+')
     let mExiste = resultat.innerHTML.indexOf('-')
     let fExiste = resultat.innerHTML.indexOf('X')
     let dExiste = resultat.innerHTML.indexOf('/')
 
+    //on tets si aucun des 4 symboles n'existe dans le résultat
     if (pExiste == -1 && mExiste == -1 && fExiste == -1 && dExiste == -1) {
         resultat.innerHTML += sym
     }
