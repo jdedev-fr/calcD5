21b921e2383b7e8a2cc9d360d108a5d9c93276dc
JdeDev <julien@jdedev.fr>
2021-09-28 15:43:38 +0200

    ajout gestion =

--- js/calc.js
+++ js/calc.js
@@ -23,3 +23,39 @@ function appuisClear() {
     //On vide la balise résultat
     resultat.innerHTML = ""
 }
+
+function egal() {
+    //On récupère la position des symboles +-*/ dans la balise HTML résultat, 
+    //si la fonction répond -1 la balise résultat ne contient pas le symbole
+    let pExiste = resultat.innerHTML.indexOf('+')
+    let mExiste = resultat.innerHTML.indexOf('-')
+    let fExiste = resultat.innerHTML.indexOf('X')
+    let dExiste = resultat.innerHTML.indexOf('/')
+
+    if (pExiste > -1) {
+        //Il y a un plus, on découpe la chaine avec + et on additionne
+        let nombres = resultat.innerHTML.split('+')
+        resultat.innerHTML = (+nombres[0]) + (+nombres[1])
+    }
+    else {
+        if (mExiste > -1) {
+            //Il y a un moins, on découpe la chaine avec - et on soustrait
+            let nombres = resultat.innerHTML.split('-')
+            resultat.innerHTML = (+nombres[0]) - (+nombres[1])
+        }
+        else {
+            if (fExiste > -1) {
+                //Il y a un fois, on découpe la chaine avec X et on multiplie
+                let nombres = resultat.innerHTML.split('X')
+                resultat.innerHTML = (+nombres[0]) * (+nombres[1])
+            }
+            else {
+                if (dExiste > -1) {
+                    //Il y a un diviser, on découpe la chaine avec / et on divise
+                    let nombres = resultat.innerHTML.split('/')
+                    resultat.innerHTML = (+nombres[0]) / (+nombres[1])
+                }
+            }
+        }
+    }
+}
